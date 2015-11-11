e2291463ffe52b9c2b033240ab8347bbd07acf7e
Sam Betts <sam@code-smash.net>
2015-11-11 14:11:26 +0000

    Use the default vnics and both uplinks
    Change-Id: I8f2b65f163648ed4f08bfa7fcaa47b879d17d34e

--- cisco_ironic_contrib/tests/unit/ironic/cimc/test_common.py
+++ cisco_ironic_contrib/tests/unit/ironic/cimc/test_common.py
@@ -43,20 +43,20 @@ class AddVnicTestCase(test_common.CIMCBaseTestCase):
                 mo.__getitem__.return_value.Dn = "DN"
                 handle.xml_query.return_value.error_code = None
 
-                dn = "DN/host-eth-name"
+                dn = "DN/host-eth-eth0"
 
-                common.add_vnic(task, "name", "mac_address", 600, pxe)
+                common.add_vnic(task, 0, "mac_address", 600, pxe=pxe)
 
                 mock_mo.assert_any_call("adaptorEthGenProfile")
                 mock_mo.assert_any_call("adaptorHostEthIf")
 
-                first_mock.set_attr.assert_any_call("name", "name")
+                first_mock.set_attr.assert_any_call("name", "eth0")
                 first_mock.set_attr.assert_any_call("mtu", "1500")
                 first_mock.set_attr.assert_any_call(
                     "pxeBoot", "enabled" if pxe else "disabled")
                 first_mock.set_attr.assert_any_call("Dn", dn)
                 first_mock.set_attr.assert_any_call("mac", "mac_address")
-                first_mock.set_attr.assert_any_call("uplinkPort", "1")
+                first_mock.set_attr.assert_any_call("uplinkPort", "0")
 
                 second_mock.set_attr.assert_any_call("vlanMode", "ACCESS")
                 second_mock.set_attr.assert_any_call("vlan", "600")
@@ -74,27 +74,13 @@ class AddVnicTestCase(test_common.CIMCBaseTestCase):
     def test_add_vnic_pxe(self, mock_mo, mock_handle):
         self._test_add_vnic(mock_mo, mock_handle, pxe=True)
 
-    @mock.patch.object(imcsdk.ImcCore, 'ManagedObject', autospec=True)
-    def test_add_vnic_long_name(self, mock_mo, mock_handle):
-        with task_manager.acquire(self.context, self.node.uuid,
-                                  shared=False) as task:
-            with mock_handle(task) as handle:
-                mo = handle.get_imc_managedobject.return_value
-                mo.__getitem__.return_value.Dn = "DN"
-                handle.xml_query.return_value.error_code = None
-                dn = "DN/host-eth-namenamenamenamenamenamenamenam"
-                common.add_vnic(
-                    task, "namenamenamenamenamenamenamenamename",
-                    "mac_address", 600)
-                mock_mo.return_value.set_attr.assert_any_call("Dn", dn)
-
     def test_add_vnic_fail(self, mock_handle):
         with task_manager.acquire(self.context, self.node.uuid,
                                   shared=False) as task:
             with mock_handle(task) as handle:
                 handle.xml_query.return_value.error_code = "123456"
                 self.assertRaises(imcsdk.ImcException, common.add_vnic,
-                                  task, "name", "mac_address", 600)
+                                  task, 0, "mac_address", 600)
 
 
 @mock.patch.object(cimc_common, 'cimc_handle', autospec=True)
@@ -107,9 +93,9 @@ class DeleteVnicTestCase(test_common.CIMCBaseTestCase):
                 mo = handle.get_imc_managedobject.return_value
                 mo.__getitem__.return_value.Dn = "DN"
 
-                common.delete_vnic(task, "name")
+                common.delete_vnic(task, 4)
 
-                expected_params = {"Dn": "DN/host-eth-name"}
+                expected_params = {"Dn": "DN/host-eth-eth4"}
                 handle.remove_imc_managedobject.assert_called_once_with(
                     None, class_id="adaptorHostEthIf", params=expected_params)
 
@@ -123,23 +109,19 @@ class DeleteVnicTestCase(test_common.CIMCBaseTestCase):
                     imcsdk.ImcException("Boom"))
 
                 self.assertRaises(imcsdk.ImcException,
-                                  common.delete_vnic, task, "name")
+                                  common.delete_vnic, task, 4)
 
-                expected_params = {"Dn": "DN/host-eth-name"}
+                expected_params = {"Dn": "DN/host-eth-eth4"}
                 handle.remove_imc_managedobject.assert_called_once_with(
                     None, class_id="adaptorHostEthIf", params=expected_params)
 
-    def test_delete_vnic_long_name(self, mock_handle):
+    @mock.patch.object(common, 'clean_vnic', autospec=True)
+    def test_delete_vnic_with_default_vnic(self, mock_clean, mock_handle):
         with task_manager.acquire(self.context, self.node.uuid,
                                   shared=False) as task:
             with mock_handle(task) as handle:
                 mo = handle.get_imc_managedobject.return_value
                 mo.__getitem__.return_value.Dn = "DN"
 
-                common.delete_vnic(
-                    task, "namenamenamenamenamenamenamenamename")
-
-                expected_params = {
-                    "Dn": "DN/host-eth-namenamenamenamenamenamenamenam"}
-                handle.remove_imc_managedobject.assert_called_once_with(
-                    None, class_id="adaptorHostEthIf", params=expected_params)
+                common.delete_vnic(task, 0)
+                mock_clean.assert_called_once_with(task, 0)
