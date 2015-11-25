fb08e19ec34e0cb43a798e6c460e7d1afc4667c3
Sam Betts <sam@code-smash.net>
2015-11-25 13:00:47 +0000

    Fully embrace the network provider system
    This has a requirement on network providers being added in Ironic
    
    Change-Id: Id7ef83299537fff66e4bc52c19864472f5b03f43
    Depends-On: If9560a7845fa586fe2c3c7b5b2c8ed5e68caafa2

--- cisco_ironic_contrib/tests/unit/ironic/cimc/test_boot.py
+++ cisco_ironic_contrib/tests/unit/ironic/cimc/test_boot.py
@@ -27,6 +27,7 @@ from ironic.objects import port
 from ironic.tests.unit.drivers.modules.cimc import test_common
 
 from cisco_ironic_contrib.ironic.cimc import boot
+from cisco_ironic_contrib.ironic.cimc import network
 
 CONF = cfg.CONF
 
@@ -34,6 +35,8 @@ CONF = cfg.CONF
 def with_task(func):
 
     def wrapper(self, *args, **kwargs):
+        self.node.network_provider = "cimc_network_provider"
+        self.node.save()
         with task_manager.acquire(self.context,
                                   self.node.uuid,
                                   shared=False) as task:
@@ -108,13 +111,14 @@ class PXEBootTestCase(test_common.CIMCBaseTestCase):
     @mock.patch.object(pxe, '_cache_ramdisk_kernel', autospec=True)
     @mock.patch.object(boot, 'get_provisioning_vifs', autospec=True)
     @mock.patch.object(dhcp_factory.DHCPFactory, 'update_dhcp', autospec=True)
-    @mock.patch.object(boot.network_provider, 'add_provisioning_network',
+    @mock.patch.object(network.NetworkProvider, 'add_provisioning_network',
                        autospec=True)
     def test_prepare_ramdisk(self, mock_add_pro, mock_dhcp, mock_get_vifs,
                              mock_cache_ramdisk, mock_create_pxe,
                              mock_get_instance, mock_build_pxe,
                              mock_get_deploy, mock_set_boot):
         self.node.provision_state = states.DEPLOYING
+        self.node.network_provider = "cimc_network_provider"
         self.node.save()
 
         vifs = {self.node.uuid: 'vif_uuid'}
@@ -127,7 +131,7 @@ class PXEBootTestCase(test_common.CIMCBaseTestCase):
             task.driver.boot.prepare_ramdisk(task, {'foo': 'bar'})
 
             mock_set_boot.assert_called_once_with(task, boot_devices.PXE)
-            mock_add_pro.assert_called_once_with(task)
+            mock_add_pro.assert_called_once_with(mock.ANY, task)
             mock_get_deploy.assert_called_once_with(task.node)
             mock_build_pxe.assert_called_once_with(
                 task, mock_get_deploy.return_value)
@@ -140,9 +144,9 @@ class PXEBootTestCase(test_common.CIMCBaseTestCase):
             mock_dhcp.assert_called_once_with(mock.ANY, task, opts, vifs)
 
     @mock.patch.object(pxe.PXEBoot, 'prepare_instance', autospec=True)
-    @mock.patch.object(boot.network_provider, 'remove_provisioning_network',
+    @mock.patch.object(network.NetworkProvider, 'remove_provisioning_network',
                        autospec=True)
-    @mock.patch.object(boot.network_provider, 'configure_tenant_networks',
+    @mock.patch.object(network.NetworkProvider, 'configure_tenant_networks',
                        autospec=True)
     @with_task
     def test_prepare_instance(self, task, mock_config, mock_unplug,
@@ -150,12 +154,12 @@ class PXEBootTestCase(test_common.CIMCBaseTestCase):
         task.driver.boot.prepare_instance(task)
         mock_prepare.assert_called_once_with(mock.ANY, task)
         self.assertFalse(mock_unplug.called)
-        mock_config.assert_called_once_with(task)
+        mock_config.assert_called_once_with(mock.ANY, task)
 
     @mock.patch.object(pxe.PXEBoot, 'prepare_instance', autospec=True)
-    @mock.patch.object(boot.network_provider, 'remove_provisioning_network',
+    @mock.patch.object(network.NetworkProvider, 'remove_provisioning_network',
                        autospec=True)
-    @mock.patch.object(boot.network_provider, 'configure_tenant_networks',
+    @mock.patch.object(network.NetworkProvider, 'configure_tenant_networks',
                        autospec=True)
     @with_task
     def test_prepare_instance_local(self, task, mock_config, mock_unplug,
@@ -163,23 +167,27 @@ class PXEBootTestCase(test_common.CIMCBaseTestCase):
         task.node.instance_info['capabilities'] = {"boot_option": "local"}
         task.driver.boot.prepare_instance(task)
         mock_prepare.assert_called_once_with(mock.ANY, task)
-        mock_unplug.assert_called_once_with(task)
-        mock_config.assert_called_once_with(task)
+        mock_unplug.assert_called_once_with(mock.ANY, task)
+        mock_config.assert_called_once_with(mock.ANY, task)
 
     @mock.patch.object(pxe.PXEBoot, 'clean_up_ramdisk', autospec=True)
-    @mock.patch.object(boot.network_provider, 'remove_provisioning_network',
+    @mock.patch.object(network.NetworkProvider, 'remove_provisioning_network',
+                       autospec=True)
+    @mock.patch.object(network.NetworkProvider, 'remove_cleaning_network',
                        autospec=True)
     @with_task
-    def test_clean_up_ramdisk(self, task, mock_remove, mock_clean):
+    def test_clean_up_ramdisk(self, task, mock_remove_clean, mock_remove_pro,
+                              mock_clean):
         task.driver.boot.clean_up_ramdisk(task)
         mock_clean.assert_called_once_with(mock.ANY, task)
-        mock_remove.assert_called_once_with(task)
+        mock_remove_pro.assert_called_once_with(mock.ANY, task)
+        mock_remove_clean.assert_called_once_with(mock.ANY, task)
 
     @mock.patch.object(pxe.PXEBoot, 'clean_up_instance', autospec=True)
-    @mock.patch.object(boot.network_provider, 'unconfigure_tenant_networks',
+    @mock.patch.object(network.NetworkProvider, 'unconfigure_tenant_networks',
                        autospec=True)
     @with_task
     def test_clean_up_instance(self, task, mock_unconfig, mock_clean):
         task.driver.boot.clean_up_instance(task)
         mock_clean.assert_called_once_with(mock.ANY, task)
-        mock_unconfig.assert_called_once_with(task)
+        mock_unconfig.assert_called_once_with(mock.ANY, task)
