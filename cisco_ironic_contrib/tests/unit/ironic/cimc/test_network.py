439bd601fd4ffd525885bc9206a1f10682277958
Sam Betts <sam@code-smash.net>
2015-11-25 13:00:47 +0000

    Fully embrace the network provider system
    Change-Id: Id7ef83299537fff66e4bc52c19864472f5b03f43
    Depends-On: If9560a7845fa586fe2c3c7b5b2c8ed5e68caafa2

--- cisco_ironic_contrib/tests/unit/ironic/cimc/test_network.py
+++ cisco_ironic_contrib/tests/unit/ironic/cimc/test_network.py
@@ -16,10 +16,10 @@ import mock
 
 from oslo_config import cfg
 
+from ironic.common import network as common_net
 from ironic.common import states
 from ironic.conductor import task_manager
 from ironic.conductor import utils as manager_utils
-from ironic.dhcp import neutron
 from ironic import objects
 from ironic.tests.unit.drivers.modules.cimc import test_common
 
@@ -45,7 +45,7 @@ class PXEBootTestCase(test_common.CIMCBaseTestCase):
     @mock.patch.object(manager_utils, 'node_power_action', autospec=True)
     @mock.patch.object(objects, 'Port', autospec=True)
     @mock.patch.object(common, 'add_vnic', autospec=True)
-    @mock.patch.object(neutron, '_build_client', autospec=True)
+    @mock.patch.object(common_net, 'get_neutron_client', autospec=True)
     @with_task
     def test_add_provisioning_network(self, task, mock__build_client,
                                       mock_add_vnic, mock_port, mock_power):
@@ -84,16 +84,17 @@ class PXEBootTestCase(test_common.CIMCBaseTestCase):
             task, 0, 'fake_address', 600, True)
         mock_port.assert_called_once_with(task.context, node_id=task.node.id,
                                           address='fake_address',
+                                          pxe_enabled=True,
                                           extra={
                                               "vif_port_id": 'fake_id',
                                               "type": "deploy",
-                                              "state": "ACTIVE",
+                                              "state": "UP",
                                               "vnic_id": 0})
         mock_port.return_value.create.assert_called_once_with()
 
     @mock.patch.object(manager_utils, 'node_power_action', autospec=True)
     @mock.patch.object(objects, 'Port', autospec=True)
-    @mock.patch.object(neutron, '_build_client', autospec=True)
+    @mock.patch.object(common_net, 'get_neutron_client', autospec=True)
     @mock.patch.object(common, 'delete_vnic', autospec=True)
     @with_task
     def test_remove_provisioning_network(self, task, mock_delete_vnic,
