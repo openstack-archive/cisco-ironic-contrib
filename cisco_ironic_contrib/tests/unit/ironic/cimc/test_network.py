74f5592f284e0263e4859fed6bd233a8506552de
Sam Betts <sam@code-smash.net>
2015-12-04 17:17:15 +0000

    Enable mode 4 on portgroups
    Change-Id: I29775f4fb594b5dc02612e22ea3ae2dcf4708b9f

--- cisco_ironic_contrib/tests/unit/ironic/cimc/test_network.py
+++ cisco_ironic_contrib/tests/unit/ironic/cimc/test_network.py
@@ -136,8 +136,180 @@ class PXEBootTestCase(test_common.BaseTestCase):
         client.delete_port.assert_called_once_with('port2')
         portMock2.destroy.assert_called_once_with()
 
-    def test_configure_tenant_networks(self):
-        pass
+    @mock.patch.object(common, 'add_vnic', autospec=True)
+    @mock.patch.object(objects, 'Port', autospec=True)
+    @with_task
+    def test_configure_tenant_networks(self, task, mock_port, mock_add):
+        port1 = {
+            'address': "address1",
+            'pxe_enabled': False,
+            'extra': {
+                'vif_port_id': 'port1',
+                'type': 'deploy',
+                'seg_id': 600,
+                'state': "DOWN"
+            }
+        }
+        portMock1 = mock.MagicMock()
+        portMock1.__getitem__.side_effect = lambda item: port1[item]
+        portMock1.get.side_effect = lambda item: port1.get(item)
+
+        port2 = {
+            'address': "address2",
+            'pxe_enabled': False,
+            'extra': {
+                'vif_port_id': 'port2',
+                'type': 'tenant',
+                'seg_id': 601,
+                'state': "DOWN"
+            }
+        }
+        portMock2 = mock.MagicMock()
+        portMock2.__getitem__.side_effect = lambda item: port2[item]
+        portMock2.get.side_effect = lambda item: port2.get(item)
+
+        port3 = {
+            'address': "address3",
+            'pxe_enabled': False,
+            'extra': {
+                'vif_port_id': 'port3',
+                'type': 'tenant',
+                'seg_id': 602,
+                'state': "DOWN"
+            }
+        }
+        portMock3 = mock.MagicMock()
+        portMock3.__getitem__.side_effect = lambda item: port3[item]
+        portMock3.get.side_effect = lambda item: port3.get(item)
+
+        mock_port.list_by_node_id.return_value = [portMock1,
+                                                  portMock2,
+                                                  portMock3]
+
+        provider = network.NetworkProvider()
+        provider.configure_tenant_networks(task)
+
+        calls = [
+            mock.call(task, 0, "address2", 601, pxe=False),
+            mock.call(task, 1, "address3", 602, pxe=False)
+        ]
+        mock_add.assert_has_calls(calls)
+
+    @mock.patch.object(objects, 'Portgroup', autospec=True)
+    @mock.patch.object(common, 'add_vnic', autospec=True)
+    @mock.patch.object(objects, 'Port', autospec=True)
+    @with_task
+    def test_configure_tenant_networks_vpc(self, task, mock_port, mock_add,
+                                           mock_pg):
+        pg1 = {
+            'address': 'pg_address',
+            'extra': {
+                'vif_port_id': 'vif',
+                'mode': 4
+            }
+        }
+        pgmock1 = mock.MagicMock()
+        pgmock1.__getitem__.side_effect = lambda item: pg1[item]
+        pgmock1.get.side_effect = lambda item: pg1.get(item)
+
+        pg2 = {
+            'address': 'pg_address',
+            'extra': {
+                'vif_port_id': 'vif',
+                'mode': 0
+            }
+        }
+        pgmock2 = mock.MagicMock()
+        pgmock2.__getitem__.side_effect = lambda item: pg2[item]
+        pgmock2.get.side_effect = lambda item: pg2.get(item)
+
+        mock_pg.get.side_effect = lambda con, n: pgmock1 if n == 1 else pgmock2
+
+        port1 = {
+            'address': "address1",
+            'pxe_enabled': False,
+            'extra': {
+                'type': 'deploy',
+                'seg_id': 600,
+                'state': "DOWN"
+            }
+        }
+        portMock1 = mock.MagicMock()
+        portMock1.__getitem__.side_effect = lambda item: port1[item]
+        portMock1.get.side_effect = lambda item: port1.get(item)
+
+        port2 = {
+            'address': "address2",
+            'pxe_enabled': False,
+            'portgroup_id': 1,
+            'extra': {
+                'type': 'tenant',
+                'seg_id': 601,
+                'state': "DOWN"
+            }
+        }
+        portMock2 = mock.MagicMock()
+        portMock2.__getitem__.side_effect = lambda item: port2[item]
+        portMock2.get.side_effect = lambda item: port2.get(item)
+
+        port3 = {
+            'address': "address3",
+            'pxe_enabled': False,
+            'portgroup_id': 1,
+            'extra': {
+                'type': 'tenant',
+                'seg_id': 601,
+                'state': "DOWN"
+            }
+        }
+        portMock3 = mock.MagicMock()
+        portMock3.__getitem__.side_effect = lambda item: port3[item]
+        portMock3.get.side_effect = lambda item: port3.get(item)
+
+        port4 = {
+            'address': "address2",
+            'pxe_enabled': False,
+            'portgroup_id': 2,
+            'extra': {
+                'type': 'tenant',
+                'seg_id': 602,
+                'state': "DOWN"
+            }
+        }
+        portMock4 = mock.MagicMock()
+        portMock4.__getitem__.side_effect = lambda item: port4[item]
+        portMock4.get.side_effect = lambda item: port4.get(item)
+
+        port5 = {
+            'address': "address3",
+            'pxe_enabled': False,
+            'portgroup_id': 2,
+            'extra': {
+                'type': 'tenant',
+                'seg_id': 602,
+                'state': "DOWN"
+            }
+        }
+        portMock5 = mock.MagicMock()
+        portMock5.__getitem__.side_effect = lambda item: port5[item]
+        portMock5.get.side_effect = lambda item: port5.get(item)
+
+        mock_port.list_by_node_id.return_value = [portMock1,
+                                                  portMock2,
+                                                  portMock3,
+                                                  portMock4,
+                                                  portMock5]
+
+        provider = network.NetworkProvider()
+        provider.configure_tenant_networks(task)
+
+        calls = [
+            mock.call(task, 0, "address2", None, pxe=False),
+            mock.call(task, 1, "address3", None, pxe=False),
+            mock.call(task, 2, "address2", 602, pxe=False),
+            mock.call(task, 3, "address3", 602, pxe=False),
+        ]
+        mock_add.assert_has_calls(calls)
 
     def test_unconfigure_tenant_networks(self):
         pass
